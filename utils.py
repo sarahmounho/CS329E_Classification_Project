533dc418b19d3d673997d371e1fbb64eff20f94e
John Dikeman <jrobdikeman@gmail.com>
2020-04-17 15:49:55 -0500

    made data_intake do the upsampling and remove anomalies

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 
-def intake_data(method=0):
+def intake_data(method = 0, upsample = True):
+    print("TEST")
     data_unedited = pd.read_csv("data_unedited.csv")
     data = data_unedited[:197]
     data = data.drop(columns = "Unnamed: 0")
@@ -15,5 +16,26 @@ def intake_data(method=0):
     data.rename(columns = {'death = 1 ': 'death'}, inplace=True)
     imputation_methods = [data.mean, data.median]
 
-    data.fillna(imputation_methods[method](), inplace=True) 
-    return data
+    data.fillna(imputation_methods[method](), inplace=True)
+
+    # these are the anomalies detected in the exploration notebook file 
+    indexes_to_drop = [70, 135, 162, 189]
+    for ind in indexes_to_drop:
+      data = data.drop(data.index[ind])
+
+    data_x = data.drop(["death","days.1","ventilator weaning = 1", "VFD ","days"], axis=1)
+    data_y = data["death"]
+    column_names = list(data_x.columns)
+    
+    if not upsample:
+        return (data_x, data_y)
+    try:
+        from imblearn import over_sampling
+    except Exception as e:
+        print(f"looks like imblearn isn't installed! skipping upsampling: {str(e)}")
+        
+    oversample = over_sampling.SMOTE()
+    X_SMOTE, Y_SMOTE = oversample.fit_resample(data_x, data_y)
+    x_frame = pd.DataFrame(X_SMOTE)
+    x_frame.columns = column_names
+    return (x_frame, Y_SMOTE)
